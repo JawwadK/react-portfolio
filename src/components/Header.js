2e820d529f1909d75a7f215044441fef26457096
Jawwad Khan <jawwad1809@gmail.com>
2021-01-13 22:20:35 -0500

    added stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ export default class Header extends Component {
                 <header id="home">
                     <nav id="nav-wrap">
                         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                        {/* eslint-disable-next-line */}
                         <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
                         <ul id="nav" className="nav">
                             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,7 +14,7 @@ export default class Portfolio extends Component {
                                         <div className="columns portfolio-item">
                                             <div className="item-wrap">
                                                 <a href="#modal-01">
-                                                    <img src={`${item.imgurl}`} className="item-img"/>
+                                                    <img src={`${item.imgurl}`} className="item-img" alt=""/>
                                                     <div className="overlay">
                                                         <div className="portfolio-item-meta">
                                                             <h5>{item.name}</h5>
